3acd33b20282733edc879d8fef74de605d9ae6c3
Emin Talha Çelik <86913426+eminbaba7231@users.noreply.github.com>
2024-10-31 13:05:43 +0300

    Update ex09.c

--- ex09.c
+++ ex09.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
+    int main(){
+int numbers;
+int count = 0;
+int total_sum = 0;
 
+printf("Enter nummbers(if you want to finish, write -1):\n");
+
+      while (1) {
+        scanf("%d", &numbers);
+
+        if (numbers == -1) break;
+       
+        count++;
+        total_sum += numbers;
+        }
+    double average = (count > 0) ? (double)total_sum / count : 0.0;
+
+      printf("Count: %d\n", count);
+      printf("Sum of all numbers: %d\n", total_sum);
+      printf("Average of all numbers: %.2f\n", average);
+
+      return 0;
+    }
