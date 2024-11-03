98fe6ed7395afb4e2f8fcf68acc7738dfff6470f
Emin Talha Çelik <86913426+eminbaba7231@users.noreply.github.com>
2024-11-03 17:58:39 +0300

    Update ex14.c

--- ex14.c
+++ ex14.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+int fibonacci(int n) {
+    if (n <= 0) {
+        return 0; 
+    } else if (n == 1) {
+        return 1; 
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2); 
+    }
+}
+
+void displayFibonacciUpTo(int n) {
+    printf("Fibonacci numbers up to position %d:\n", n);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    displayFibonacciUpTo(n);
+
+    printf("The %dth Fibonacci number is: %d\n", n, fibonacci(n));
+
+    return 0;
+}
