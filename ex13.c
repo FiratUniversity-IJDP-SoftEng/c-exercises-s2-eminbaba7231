92fc22b73c27c8e17b037d747ffd114ffd239e3d
Emin Talha Çelik <86913426+eminbaba7231@users.noreply.github.com>
2024-11-03 17:55:00 +0300

    Update ex13.c

--- ex13.c
+++ ex13.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+unsigned long long factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1; 
+    } else {
+        return n * factorial(n - 1); 
+    }
+}
+
+int main() {
+    int number;
+
+    printf("Enter a number (less than 50): ");
+    scanf("%d", &number);
+
+    if (number < 0 || number >= 50) {
+        printf("Please enter a number between 0 and 49.\n");
+        return 1;
+    }
+
+    unsigned long long result = factorial(number);
+    
+    printf("Factorial of %d is %llu\n", number, result);
+
+    return 0;
+}
