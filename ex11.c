6099f7e835b8a14348c01958bdd9e880dc367ab7
Emin Talha Çelik <86913426+eminbaba7231@users.noreply.github.com>
2024-11-03 16:48:38 +0300

    Update ex11.c

--- ex11.c
+++ ex11.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+#include <stdio.h>
+
+int main(){
+  int size = 5;
+
+  for (int i = 0; i < size; i++){
+    for (int j = 0; j < size; j++){
+
+         if (j == i || j == (size - 1 - i)) {
+             printf("*");
+         } else {
+           printf(" ");
+         }
+    }
+  printf("\n");
+  }
+  return 0;
+}
